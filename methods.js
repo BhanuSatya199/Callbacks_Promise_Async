0d9bde1a9eeb66adaa34b52c5a3446710a2d073b
BhanuSatya199 <153795402+BhanuSatya199@users.noreply.github.com>
2025-12-06 11:42:01 +0530

    Methods

--- methods.js
+++ methods.js
@@ -10,6 +10,15 @@ app.post('/add-user',(req,res)=>{
     //logic to add data into your database
     res.send("data added");
 })
+app.put('/edit-user',(req,res)=>{
+     let data=req.body;
+    console.log(data);
+    res.send("edited-user");
+})
+app.delete('/delete-user',(req,res)=>{
+    console.log("It is Working");
+    res.send("deleted-user");
+})
 app.listen(7000,()=>{
     console.log("server is running at 7000");
 })
